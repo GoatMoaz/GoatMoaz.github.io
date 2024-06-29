dbcf027e94457f980fba999c28d4ef0abdff3280
Moaz Ayman <ma5746@fayoum.edu.eg>
2024-06-29 23:49:50 +0300

    Initial commit

---
+++ components/Content.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+    <div class="flex-col flex items-center justify-center mx-80">
+      <div class="flex items-center justify-center gap-7">
+        <h1 class="text-4xl text-center text-white font-bold">
+          Hello, I'm <span class="text-[#00C48D]">Moaz Ayman</span>
+        </h1>
+        <img
+          src="https://nuxt-portfolio-starter.netlify.app/_nuxt/img/developer.721e2ae.svg"
+          alt=""
+        />
+      </div>
+      <p class="text-center text-[#cbd5e0] text-lg my-4">
+        I'm a passionate computer science student from Egypt dedicated to
+        mastering full stack development. With a focus on <span class="text-[#00C48D]">Node.js</span> for backend
+        development, created robust and scalable server-side applications. On
+        the frontend, I utilizes the power of <span class="text-[#00C48D]">Vue and Nuxt</span> to build dynamic,
+        user-friendly interfaces that enhance user experience. My commitment to
+        learning and applying modern web technologies positions him as a
+        promising developer in the tech industry.
+      </p>
+      <div class="flex justify-evenly items-center border-b w-full pb-4">
+        <img
+          src="https://nuxt-portfolio-starter.netlify.app/_nuxt/img/code_review.fb0d488.svg"
+          class="w-56"
+        />
+        <img
+          src="https://nuxt-portfolio-starter.netlify.app/_nuxt/img/mind_map.552134e.svg"
+          class="w-56"
+        />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+</script>
+
+<style scoped>
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="h-screen bg-[#0D2438]">
+        <Nav />
+        <Content />
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+
+</style>
