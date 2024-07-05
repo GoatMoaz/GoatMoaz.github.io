4ce1b7ccf171bb82905c0cb461b39733a3e68f3b
Moaz Ayman <ma5746@fayoum.edu.eg>
2024-07-05 17:22:00 +0300

    Add nitro configuration for prerendering and deploy script

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,4 +2,10 @@
 export default defineNuxtConfig({
   devtools: { enabled: true },
   modules: ["@nuxtjs/tailwindcss", "@nuxt/image", "@vueuse/motion/nuxt"],
+  nitro: {
+    prerender: {
+      // Workaround for "Error: [404] Page not found: /manifest.json"
+      failOnError: false,
+    },
+  },
 })
